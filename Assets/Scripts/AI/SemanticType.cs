c65217d5aca0234ab3933d936b114ceb19d2a362
Bill Marcy <wgmarcy@gmail.com>
2020-04-01 15:18:27 -0400

    add comments explaining the function of types

--- Assets/Scripts/AI/SemanticType.cs
+++ Assets/Scripts/AI/SemanticType.cs
@@ -2,12 +2,40 @@ using System;
 using System.Text;
 using System.Collections.Generic;
 
+// the type of an expression.
+// The type determines the kind of thing an expression refers to (semantics).
+// 
+// expressions of type 'e' refer to individuals like Bob or Mount Everest,
+// 
+// expressions of type 't' refer to truth values, 1 or 0,
+// Donald Trump is president = 1, Bob Dole is president = 0
+// 
+// expressions of type 'e -> t' refer to predicates, which either
+// hold or don't hold of individuals i.e. "runs", or "green"
+// 
+// they're a function that maps an individual to
+// 1 if the predicate holds
+//   (so runs(Bob) -> 1 because Bob does run) and
+// 0 if the predicate doesn't hold
+//   (so runs(Anthony) -> 0 because Anthony doesn't run)
+//   
+// the grammar of the language also determines what expressions can combine. 
+// i.e. runs can only combine with expressions of type 'e' because
+// they consume an individual as input.
+// i.e. runs(Bob) is well-formed but runs(green) is not.
+// 
+// expressions with a functional type combine with expressions of their
+// input types to yield an expression of the output type.
+// expressions can also partially apply to some but not all of their inputs.
 public abstract class SemanticType {
+    // references to the atomic types.
     public static readonly AtomicType INDIVIDUAL = new E();
     public static readonly AtomicType TRUTH_VALUE = new T();
     public static readonly AtomicType CONFORMITY_VALUE = new C();
     public static readonly AtomicType ASSERTION = new A();
     public static readonly AtomicType QUESTION = new Q();
+
+    // references to common functional types.
     public static readonly FunctionalType PREDICATE =
         new FunctionalType(new SemanticType[]{INDIVIDUAL}, TRUTH_VALUE);
     public static readonly FunctionalType RELATION_2 =
@@ -15,41 +43,6 @@ public abstract class SemanticType {
 }
 
 public abstract class AtomicType : SemanticType {}
-
-// individuals
-public class E : AtomicType {
-    public override string ToString() {
-        return "e";
-    }
-}
-
-// truth values
-public class T : AtomicType {
-    public override string ToString() {
-        return "t";
-    }
-}
-
-// conformity values
-public class C : AtomicType {
-    public override string ToString() {
-        return "c";
-    }
-}
-
-// question (potentially redundant)
-public class Q : AtomicType {
-    public override string ToString() {
-        return "q";
-    }
-}
-
-// assertion
-public class A : AtomicType {
-    public override string ToString() {
-        return "a";
-    }
-}
 
 public class FunctionalType : SemanticType {
     private SemanticType[] input;
@@ -117,5 +110,40 @@ public class FunctionalType : SemanticType {
         s.Append(" -> " + output.ToString() + ")");
 
         return s.ToString();
+    }
+}
+
+// individuals
+public class E : AtomicType {
+    public override string ToString() {
+        return "e";
+    }
+}
+
+// truth values
+public class T : AtomicType {
+    public override string ToString() {
+        return "t";
+    }
+}
+
+// conformity values
+public class C : AtomicType {
+    public override string ToString() {
+        return "c";
+    }
+}
+
+// question (potentially redundant)
+public class Q : AtomicType {
+    public override string ToString() {
+        return "q";
+    }
+}
+
+// assertion
+public class A : AtomicType {
+    public override string ToString() {
+        return "a";
     }
 }
