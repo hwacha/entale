a4726ea79ac3328d0219294de6afab8cc97cf30e
Souren Papazian <souren@papazian.info>
2020-04-08 01:02:22 -0400

    Draft of radial menu with highlighting but no click

---
+++ Assets/Scripts/UI/RadialMenuItem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using static SemanticType;
+using static Expression;
+
+public class RadialMenuItem : MonoBehaviour
+{
+    Outline outline;
+    Image icon;
+
+    void Awake() {
+        outline = GetComponent<Outline>();
+        icon = GetComponent<Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Unhighlight();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Unhighlight() {
+        outline.enabled = false;
+    }
+
+    public void Highlight() {
+        outline.enabled = true;
+    }
+
+    public void setIcon(SemanticType semanticType) {
+        if (semanticType == INDIVIDUAL) {
+            icon.sprite = Resources.Load<Sprite>("Sprites/red_circle") as Sprite;
+        } else if (semanticType == PREDICATE) {
+            icon.sprite = Resources.Load<Sprite>("Sprites/green_circle") as Sprite;
+        } else if (semanticType == RELATION_2) {
+            icon.sprite = Resources.Load<Sprite>("Sprites/orange_circle") as Sprite;
+        } else if (semanticType == TRUTH_VALUE) {
+            icon.sprite = Resources.Load<Sprite>("Sprites/blue_circle") as Sprite;
+        } else if (semanticType == TRUTH_FUNCTION) {
+            icon.sprite = Resources.Load<Sprite>("Sprites/pink_circle") as Sprite;
+        }
+        else {
+            Debug.Log("No sprite available for this semantic type!");
+        }
+    }
+}
--- Assets/Scripts/UI/WordSelectMenu.cs
+++ Assets/Scripts/UI/WordSelectMenu.cs
@@ -8,48 +8,73 @@ using static SemanticType;
 using static Expression;
 
 public class WordSelectMenu : MonoBehaviour {
-
-    const double INITIAL_OFFSET = Math.PI / 2;
+    public RadialMenuItem radialMenuItemPrefab;
     const double RADIUS = 100;
+    Vector2 SCREEN_CENTER = new Vector2(Screen.width, Screen.height);
 
-    Dictionary<SemanticType, HashSet<Constant>> Lexicon;
+    Dictionary<SemanticType, HashSet<Constant>> Lexicon = new Dictionary<SemanticType, HashSet<Constant>> {
+        [INDIVIDUAL] = new HashSet<Constant> { ALICE.Head as Constant, BOB.Head as Constant },
+        [PREDICATE] = new HashSet<Constant> { RED.Head as Constant, BLUE.Head as Constant },
+        [TRUTH_VALUE] = new HashSet<Constant> { VERUM.Head as Constant },
+        [RELATION_2] = new HashSet<Constant> { AT.Head as Constant, IDENTITY.Head as Constant },
+        [TRUTH_FUNCTION] = new HashSet<Constant> { AT.Head as Constant, IDENTITY.Head as Constant }
+    };
+
+    List<RadialMenuItem> radialMenuItems = new List<RadialMenuItem>(); 
+    int currentSliceIndex = -1;
+    double INITIAL_ANGLE_OFFSET = Math.PI / 2f;
 
     // Start is called before the first frame update
     void Start() {
-        // Create lexicon
-        Lexicon = new Dictionary<SemanticType, HashSet<Constant>> {
-            [INDIVIDUAL] = new HashSet<Constant> { ALICE.Head as Constant, BOB.Head as Constant },
-            [PREDICATE] = new HashSet<Constant> { RED.Head as Constant, BLUE.Head as Constant },
-            [TRUTH_VALUE] = new HashSet<Constant> { VERUM.Head as Constant },
-            [RELATION_2] = new HashSet<Constant> { AT.Head as Constant, IDENTITY.Head as Constant }
-        };
+        DrawMenu(INITIAL_ANGLE_OFFSET, RADIUS);
+    }
 
+    void DrawMenu(double initial_angle_offset, double radius) {
         double sliceTheta = 2.0 * Math.PI / Lexicon.Keys.Count;
-        double theta = INITIAL_OFFSET;
+        double theta = initial_angle_offset;
         foreach (SemanticType semanticType in Lexicon.Keys) {
+            theta = theta % (Math.PI * 2);
+            var radialMenuItem = Instantiate(radialMenuItemPrefab);
+            radialMenuItem.GetComponent<Transform>().SetParent(gameObject.transform);
+            radialMenuItem.GetComponent<Transform>().localPosition = new Vector2(
+                (float)(Math.Cos(theta) * radius),
+                (float)(Math.Sin(theta) * radius)
+            );
+            radialMenuItem.setIcon(semanticType);
+            radialMenuItems.Add(radialMenuItem);
             theta += sliceTheta;
-            GameObject imageContainer = new GameObject();
-            imageContainer.transform.parent = this.gameObject.transform;
-            Image typeImage = imageContainer.AddComponent<Image>();
-            if (semanticType == INDIVIDUAL) {
-                typeImage.sprite = Resources.Load<Sprite>("Sprites/individual") as Sprite;
-            } else if (semanticType == PREDICATE) {
-                typeImage.sprite = Resources.Load<Sprite>("Sprites/predicate") as Sprite;
-            } else if (semanticType == RELATION_2) {
-                typeImage.sprite = Resources.Load<Sprite>("Sprites/relation_2") as Sprite;
-            } else if (semanticType == TRUTH_VALUE) {
-                typeImage.sprite = Resources.Load<Sprite>("Sprites/truth_value") as Sprite;
-            } else {
-                Debug.Log("No sprite available for this semantic type!");
-            }
-            typeImage.GetComponent<RectTransform>().localPosition = new Vector2(
-                (float)(Math.Cos(theta) * RADIUS),
-                (float)(Math.Sin(theta) * RADIUS));
         }
     }
 
     // Update is called once per frame
     void Update() {
+        int sliceIndex = GetSliceIndex(radialMenuItems.Count, INITIAL_ANGLE_OFFSET, GetMouseAngle());
+        if (sliceIndex != currentSliceIndex) {
+            foreach(var rmi in radialMenuItems) {
+                rmi.Unhighlight();
+            }
+            currentSliceIndex = sliceIndex;
+        }
+        radialMenuItems[currentSliceIndex].Highlight();
+    }
 
+    // Returns the mouse angle from the cetner of the screen relative to the x axis in counter clockwise
+    double GetMouseAngle() {
+        Vector2 mousePosition = GetMousePosition(); 
+        double angle = Mathf.Atan2(mousePosition.y, mousePosition.x);
+        return angle > 0 ? angle : (2*Math.PI + angle);
+    }
+
+    // Returns the mouse position from the center of the screen normalized by the screen's size
+    Vector2 GetMousePosition() {
+        return ((Input.mousePosition / SCREEN_CENTER) * 2) - Vector2.one; 
+    }
+
+    // If the slices are 
+    int GetSliceIndex(int sliceCount, double inital_angle_offset, double angle_in) {
+        double sliceAngle = Math.PI * 2 / sliceCount;
+        double angle = angle_in - inital_angle_offset + (sliceAngle/2);
+        angle = angle > 0 ? angle : (2*Math.PI + angle);
+        return (int) Math.Floor(angle / sliceAngle) % sliceCount;
     }
 }
