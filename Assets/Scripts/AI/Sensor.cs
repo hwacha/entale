4e2c756e6accb1f0d48c1ecad7c213661eb5eb81
Bill Marcy <wgmarcy@gmail.com>
2020-05-12 17:15:21 -0400

    Add raycasting to vision sensor

--- Assets/Scripts/AI/Sensor.cs
+++ Assets/Scripts/AI/Sensor.cs
@@ -7,6 +7,13 @@ using static Expression;
 public class Sensor : MonoBehaviour {
     public Agent Agent;
 
+    #region Constants
+        static float TIME_STEP = 0.5f;
+        static int NUM_RINGS = 10;
+        static int RAYS_PER_RING = 10;
+        static float D_THETA = (2 * Mathf.PI) / RAYS_PER_RING;
+    #endregion
+
     // @Note for separate sense modalities,
     // we'd want this to become an abstract class,
     // making some sort of abstract method like
@@ -20,14 +27,74 @@ public class Sensor : MonoBehaviour {
     // Right now, assume it's a vision module.
     public IEnumerator ReceiveStimulus() {
         while (true) {
-            var tree = GameObject.Find("tree");
-            if (Vector3.Distance(tree.transform.position,
-                Agent.gameObject.transform.position) < 2) {
-                Agent.MentalState.StartCoroutine(Agent.MentalState.Assert(
+            // we'll put all the objects this sensor can see
+            // so we avoid redundancy.
+            var visibleObjects = new HashSet<GameObject>();
+
+            int layerMask = 1 << 9;
+
+            RaycastHit hit;
+            bool collided;
+
+            collided = Physics.Raycast(
+                Agent.transform.position,
+                Agent.transform.TransformDirection(Vector3.forward),
+                out hit,
+                layerMask);
+
+            // Debug.DrawRay(transform.position, Vector3.forward * 100, Color.white);
+
+            // Here, we can assert whatever information
+            // we gather from this raycast hit.
+            void OnCollision(RaycastHit theHit) {
+                visibleObjects.Add(theHit.transform.gameObject);
+                if (theHit.distance < 0.6f) {
+                    Agent.MentalState.StartCoroutine(Agent.MentalState.Assert(
                     new Expression(PERCEIVE, SELF,
-                        new Expression(AT, SELF, new Deictic(THAT, tree)))));
+                        new Expression(AT, SELF,
+                            new Deictic(THAT,
+                                theHit.transform.gameObject)))));
+                }
             }
-            yield return new WaitForSeconds(0.5f);
+
+            if (collided) {
+                OnCollision(hit);
+            }
+
+            for (int i = 1; i < NUM_RINGS + 1; i++) {
+                // @Note here, we randomize the raycast within
+                // the bounds of its bounding range within a circle.
+                // this is to remove blindspots.
+                // 
+                // TODO:
+                // We'll want to do something similar for
+                // the regions between the rings, too.
+                // 
+                float theta = Random.Range(0, i * (Mathf.PI / RAYS_PER_RING));
+                for (int j = 0; j < RAYS_PER_RING; j++) {
+                    // here, we set up the components of a conical raycast.
+                    Vector3 zDir = Agent.transform.TransformDirection(Vector3.forward) * (float) i;
+                    Vector3 xDir = Agent.transform.TransformDirection(Vector3.right) * Mathf.Cos(theta);
+                    Vector3 yDir = Agent.transform.TransformDirection(Vector3.up) * Mathf.Sin(theta);
+
+                    // Raycast
+                    collided = Physics.Raycast(
+                        Agent.transform.position,
+                        Agent.transform.TransformDirection(zDir + xDir + yDir),
+                        out hit,
+                        layerMask);
+
+                    // Debug.DrawRay(transform.position, (zDir + xDir + yDir) * 100, Color.white);
+
+                    if (collided && !visibleObjects.Contains(hit.transform.gameObject)) {
+                        OnCollision(hit);
+                    }
+
+                    theta += D_THETA;
+                }
+            }
+
+            yield return new WaitForSeconds(TIME_STEP);
         }
     }
 }
